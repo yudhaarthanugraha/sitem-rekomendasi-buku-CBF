7dbdd2fcd76b85006d8a027580bc14bc5f369482
betthink <robetszone@gmail.com>
2024-05-19 17:04:01 +0700

    feat: dashboard siswa - menampilkan list buku dari db

--- resources/views/siswa/dashboard/index.blade.php
+++ resources/views/siswa/dashboard/index.blade.php
@@ -53,7 +53,7 @@
                                             <div class="card">
                                                 <figure class="card-img-top overlay overlay-1 hover-scale"><a
                                                         href="#">
-                                                        <img src="./assets/img/photos/b4.jpg" alt="" /></a>
+                                                        <img   src="{{ $book->gambar ? asset('uploads/' . $book->gambar) : './assets/img/photos/b4.jpg' }}" alt="" /></a>
                                                     <figcaption>
                                                         <h5 class="from-top mb-0">Lihat detail</h5>
                                                     </figcaption>
@@ -93,140 +93,6 @@
                                 </div>
                             @endforeach
 
-                            <!--/.swiper-slide -->
-                            {{-- <div class="swiper-slide">
-                                <div class="item-inner">
-                                    <article>
-                                        <div class="card">
-                                            <figure class="card-img-top overlay overlay-1 hover-scale"><a href="#">
-                                                    <img src="./assets/img/photos/b5.jpg" alt="" /></a>
-                                                <figcaption>
-                                                    <h5 class="from-top mb-0">Read More</h5>
-                                                </figcaption>
-                                            </figure>
-                                            <div class="card-body">
-                                                <div class="post-header">
-                                                    <div class="post-category text-line">
-                                                        <a href="#" class="hover" rel="category">Workspace</a>
-                                                    </div>
-                                                    <!-- /.post-category -->
-                                                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark"
-                                                            href="./blog-post.html">Nullam id dolor elit id nibh</a></h2>
-                                                </div>
-                                                <!-- /.post-header -->
-                                                <div class="post-content">
-                                                    <p>Mauris convallis non ligula non interdum. Gravida vulputate convallis
-                                                        tempus vestibulum cras imperdiet nun eu dolor.</p>
-                                                </div>
-                                                <!-- /.post-content -->
-                                            </div>
-                                            <!--/.card-body -->
-                                            <div class="card-footer">
-                                                <ul class="post-meta d-flex mb-0">
-                                                    <li class="post-date ms-auto"><i
-                                                            class="uil uil-calendar-alt"></i><span>29 Mar
-                                                            2022</span></li>
-                                                </ul>
-                                                <!-- /.post-meta -->
-                                            </div>
-                                            <!-- /.card-footer -->
-                                        </div>
-                                        <!-- /.card -->
-                                    </article>
-                                    <!-- /article -->
-                                </div>
-                                <!-- /.item-inner -->
-                            </div>
-                            <!--/.swiper-slide -->
-                            {{-- <div class="swiper-slide">
-                                <div class="item-inner">
-                                    <article>
-                                        <div class="card">
-                                            <figure class="card-img-top overlay overlay-1 hover-scale"><a href="#">
-                                                    <img src="./assets/img/photos/b6.jpg" alt="" /></a>
-                                                <figcaption>
-                                                    <h5 class="from-top mb-0">Read More</h5>
-                                                </figcaption>
-                                            </figure>
-                                            <div class="card-body">
-                                                <div class="post-header">
-                                                    <div class="post-category text-line">
-                                                        <a href="#" class="hover" rel="category">Meeting</a>
-                                                    </div>
-                                                    <!-- /.post-category -->
-                                                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark"
-                                                            href="./blog-post.html">Ultricies fusce porta elit</a></h2>
-                                                </div>
-                                                <!-- /.post-header -->
-                                                <div class="post-content">
-                                                    <p>Mauris convallis non ligula non interdum. Gravida vulputate convallis
-                                                        tempus vestibulum cras imperdiet nun eu dolor.</p>
-                                                </div>
-                                                <!-- /.post-content -->
-                                            </div>
-                                            <!--/.card-body -->
-                                            <div class="card-footer">
-                                                <ul class="post-meta d-flex mb-0">
-                                                    <li class="post-date ms-auto"><i
-                                                            class="uil uil-calendar-alt"></i><span>26 Feb
-                                                            2022</span></li>
-                                                </ul>
-                                                <!-- /.post-meta -->
-                                            </div>
-                                            <!-- /.card-footer -->
-                                        </div>
-                                        <!-- /.card -->
-                                    </article>
-                                    <!-- /article -->
-                                </div>
-                                <!-- /.item-inner -->
-                            </div>
-                            <!--/.swiper-slide -->
-                            <div class="swiper-slide">
-                                <div class="item-inner">
-                                    <article>
-                                        <div class="card">
-                                            <figure class="card-img-top overlay overlay-1 hover-scale"><a href="#">
-                                                    <img src="./assets/img/photos/b7.jpg" alt="" /></a>
-                                                <figcaption>
-                                                    <h5 class="from-top mb-0">Read More</h5>
-                                                </figcaption>
-                                            </figure>
-                                            <div class="card-body">
-                                                <div class="post-header">
-                                                    <div class="post-category text-line">
-                                                        <a href="#" class="hover" rel="category">Business Tips</a>
-                                                    </div>
-                                                    <!-- /.post-category -->
-                                                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark"
-                                                            href="./blog-post.html">Morbi leo risus porta eget</a></h2>
-                                                </div>
-                                                <!-- /.post-header -->
-                                                <div class="post-content">
-                                                    <p>Mauris convallis non ligula non interdum. Gravida vulputate convallis
-                                                        tempus vestibulum cras imperdiet nun eu dolor.</p>
-                                                </div>
-                                                <!-- /.post-content -->
-                                            </div>
-                                            <!--/.card-body -->
-                                            <div class="card-footer">
-                                                <ul class="post-meta d-flex mb-0">
-                                                    <li class="post-date ms-auto"><i
-                                                            class="uil uil-calendar-alt"></i><span>7 Jan
-                                                            2022</span></li>
-
-                                                </ul>
-                                                <!-- /.post-meta -->
-                                            </div>
-                                            <!-- /.card-footer -->
-                                        </div>
-                                        <!-- /.card -->
-                                    </article>
-                                    <!-- /article -->
-                                </div>
-                                <!-- /.item-inner -->
-                            </div> --}} --}}
-                            <!--/.swiper-slide -->
                         </div>
                         <!--/.swiper-wrapper -->
                     </div>
